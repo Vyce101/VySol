8b09ad414e35058d4d1f2fffe6843fe2e79c5dce
Luke <lukejohndaniels101@gmail.com>
2026-09-24 02:13:01 +0200

    Style world collection cards and navigation

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,6 +7,7 @@ import "@fontsource/eb-garamond/400-italic.css";
 import { App } from "./App";
 import "./styles.css";
 import "./settings-redesign.css";
+import "./worlds-redesign.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
